c608f0185ac68636f24d54155e2f9a7cd0d423ce
John Brooke <jmbrooke1@sheffield.ac.uk>
2020-01-30 15:06:29 +0000

    version of runParallel handles chi correctly

--- Threadbeast/runParallel.py
+++ Threadbeast/runParallel.py
@@ -4,12 +4,12 @@ from math import *
 import numpy as np
 import os
 import concurrent.futures
-for j in range(8,16):
-    valTemp = 0.7 - j*0.07
+for j in range(0,8):
+    valTemp = 0.42 - j*0.07
     Dn = exp(valTemp)
     Dc = Dn*0.3
     for k in range(0,4):
-        Chi = 1.0 + k*0.6
+        Chi = (1.0 + k*0.6)*Dn
         logpath = "logs/Dn" + str(j) + "Chi" + str(k)
         # print(logpath)
         command="sim/build/processNoDisplay " + logpath +" 0.00001 " + str(Dn)  + " " +  str(Chi) + " " + str(Dc) + " 500000" + " 100000 " + " 0 >output &"
