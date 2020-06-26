4bbadcf8df0a4042a3206969c0e2b4086af4ab37
John Brooke <jmbrooke1@sheffield.ac.uk>
2020-06-26 14:05:58 +0100

    new version of runParallel avoids multiple writes to output

--- Threadbeast/runParallel.py
+++ Threadbeast/runParallel.py
@@ -4,17 +4,18 @@ from math import *
 import numpy as np
 import os
 import concurrent.futures
+os.system("cp centres.h logs")
 for j in range(0,8):
-    valTemp = 0.42 - j*0.07
+    valTemp = 3.6 - j*0.4
     Dn = exp(valTemp)
     Dc = Dn*0.3
     for k in range(0,4):
-        Chi = (1.0 + k*0.6)*Dn
+        Chi = (0.4 + k*0.2)*Dn
         logpath = "logs/Dn" + str(j) + "Chi" + str(k)
-        # print(logpath)
-        command="sim/build/processNoDisplay " + logpath +" 0.00001 " + str(Dn)  + " " +  str(Chi) + " " + str(Dc) + " 500000" + " 100000 " + " 0 >output &"
-        # print(command)
-        #  os.system(command)
+        print(logpath)
+        command1 = "mkdir " + logpath
+        os.system(command1)
+        command="sim/build/processNoDisplay " + logpath +" 0.0001 " + str(Dn)  + " " +  str(Chi) + " " + str(Dc) +  " 8 " + " 6.0 " + " 400000 " + " 0 > " + logpath + "/output &"
         with concurrent.futures.ProcessPoolExecutor(max_workers=32) as executor:
           executor.submit(os.system(command))
         #  executor.submit(command)
